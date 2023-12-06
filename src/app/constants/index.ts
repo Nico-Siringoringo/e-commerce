22e1e9892547815ef8e7dc1aad79cc6b07a61077
Nico-Siringoringo <arthurhirai255@gmail.com>
2023-12-06 08:08:50 +0700

    implement ecommerce functionality

---
+++ src/app/constants/index.ts
@@ -0,0 +1,47 @@
+export const inclusions = [
+  {
+    title: 'Free Shipping',
+    description: 'Free shipping for order above $150',
+    icon: '/assets/white-icons/shipping.svg',
+  },
+  {
+    title: 'Money Guarantee',
+    description: 'Within 30 days for an exchange',
+    icon: '/assets/white-icons/dollar.svg',
+  },
+  {
+    title: 'Online Support',
+    description: '24 hours a day, 7 days a week',
+    icon: '/assets/white-icons/support.svg',
+  },
+  {
+    title: 'Flexible Payment',
+    description: 'Pay with multiple credit cards',
+    icon: '/assets/white-icons/payment.svg',
+  },
+]
+
+export const profileNavItems = [
+  {
+    title: 'Personal Information',
+    url: '/account',
+    icon: '/assets/white-icons/user.svg',
+  },
+  {
+    title: 'My Purchases',
+    url: '/account/purchases',
+    icon: '/assets/white-icons/purchases.svg',
+  },
+  {
+    title: 'My Orders',
+    url: '/account/orders',
+    icon: '/assets/white-icons/orders.svg',
+  },
+  {
+    title: 'Logout',
+    url: '/logout',
+    icon: '/assets/white-icons/logout.svg',
+  },
+]
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
